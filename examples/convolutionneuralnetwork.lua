0f10424696c9ead186cf117616a1b0450cec6919
LinZichuan <linzichuan12@163.com>
2016-02-26 00:43:57 +0800

    Print report key

--- examples/convolutionneuralnetwork.lua
+++ examples/convolutionneuralnetwork.lua
@@ -195,7 +195,15 @@ train = dp.Optimizer{
    end,
    callback = function(model, report) -- called every batch
       -- the ordering here is important
-      print(model.modules)
+      for k,v in pairs(report) do
+          print(k)
+      end
+      --layers = model.modules[1].modules
+      --w2 = layers[2].weight:view(1, 64*1*5*5)
+      --w5 = layers[5].weight:view(1, 128*64*5*5)
+      --w9 = layers[9].weight:view(1, 10*2048)
+      --state = torch.cat(torch.cat(w2,w5), w9)
+      --torch.save('/home/jie/state/')
       if opt.accUpdate then
          model:accUpdateGradParameters(model.dpnn_input, model.output, opt.learningRate)
       else
