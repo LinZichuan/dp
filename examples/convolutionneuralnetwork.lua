dd2b7837a0b8f66b79e0065018c56ad8792c521c
LinZichuan <linzichuan12@163.com>
2016-02-26 21:21:32 +0800

    Update

--- examples/convolutionneuralnetwork.lua
+++ examples/convolutionneuralnetwork.lua
@@ -203,8 +203,9 @@ train = dp.Optimizer{
    end,
    callback = function(model, report) -- called every batch
       -- the ordering here is important
+      print(model)
       layers = model.modules[1].modules
-      print(layers[9].weight)
+      --print(layers[9].weight)
       w2 = layers[2].weight:view(1, 64*1*5*5)
       w5 = layers[5].weight:view(1, 128*64*5*5)
       w9 = layers[9].weight:view(1, 10*2048)
